048caa5a363e6f95ec7b4c95fc065c8cd6f1f201
A vinod kumar <asundi.vinodkumar888@gmail.com>
2021-04-08 12:57:51 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Home from "./compo/Home";
+import Home from "./Components/Home";
 
 ReactDOM.render(<Home />, document.querySelector("#root"));
